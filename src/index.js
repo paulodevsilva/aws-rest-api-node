db67f0f929ce310a0f534ba6f96a1536bdeca8a9
Paulo Silva <silva.paulohr@gmail.com>
2022-10-21 18:35:14 -0300

    feat: started application

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const port = 3000;
+
+const app = express();
+
+app.use(express.json());
+
+const users = [];
+
+app.get('/', (req, res) => res.statusCode(200).json(users));
+
+app.post('/', (req, res) => {
+  const { body } = req;
+
+  users.push(body);
+
+  res.statusCode(201).json({ message: 'New user added' });
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
