50c5b5a336a92b4fdb519bdbb1ef02f8a55523d8
Paulo Silva <silva.paulohr@gmail.com>
2022-10-22 18:35:56 -0300

    feat: add cors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 
 const port = 3000;
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 const users = [];
 
